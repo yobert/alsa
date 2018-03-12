0fcf947a72bc5d724dbd735a2dd8fe5db59cb4c3
Hannes Landeholm <hnsl@google.com>
2018-03-13 00:20:56 +0100

    add NegotiatePeriodSize()

--- negotiate.go
+++ negotiate.go
@@ -88,6 +88,28 @@ func (device *Device) NegotiateBufferSize(buffer_sizes ...int) (int, error) {
 	return 0, err
 }
 
+func (device *Device) NegotiatePeriodSize(period_sizes ...int) (int, error) {
+	var err error
+
+	for _, v := range period_sizes {
+		if !device.hwparams.IntervalInRange(paramPeriodSize, uint32(v)) {
+			err = fmt.Errorf("Period size %d out of range")
+			continue
+		}
+
+		device.hwparams.Cmask = 0
+		device.hwparams.Rmask = 0xffffffff
+		device.hwparams.SetInterval(paramPeriodSize, uint32(v), uint32(v), Integer)
+
+		err = device.refine()
+		if err == nil {
+			return v, nil
+		}
+	}
+
+	return 0, err
+}
+
 func (device *Device) BytesPerFrame() int {
 	sample_size := int(device.hwparams.Intervals[paramSampleBits-paramFirstInterval].Max) / 8
 	channels := int(device.hwparams.Intervals[paramChannels-paramFirstInterval].Max)
